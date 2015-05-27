9b037af84ef5c06558fde6a4a271bcf6d993b4a5
ktennie <Keno@p216127.vpn-dyn.FH-Koeln.DE>
2015-05-27 16:52:47 +0200

    einführen von Bar-id, redis einbindung
    Details funktionieren noch nicht! :(

--- Workshop1/app.js
+++ Workshop1/app.js
@@ -24,7 +24,7 @@ db.on('connect', function() { // Verbing zum Server hergestellt?
     console.log('connected');
 });
 
-app.post('/bars',function(req, res){
+app.post('/bars',function(req, res){ //Hinzufügen von einer Bar
     
     var newBar = req.body;
     
@@ -36,6 +36,19 @@ app.post('/bars',function(req, res){
 	});
 });
 
+app.post('/bars/:id/details', function(req, res){ //Hinzufügen der sitzplätze einer bestimmten Bar
+    var newAnzahl = req.body;
+    db.get('bars:'+req.params.id, function(err, rep){
+       if(rep){
+           db.set('bars:'+newAnzahl.id+'/details', JSON.stringify(newAnzahl),function(err, rep){
+			 res.json(newAnzahl);
+		});
+       }
+       else{
+           res.status(404).type('text').send("Die Bar mit der ID " + req.params.id + " wurde nicht gefunden");
+       }
+   });
+})
 
 app.put('/',function(req, res){
     if (!freieSitzplaetze == 0){
@@ -44,16 +57,29 @@ app.put('/',function(req, res){
     }
 	res.send(belegteSitzplaetze + " Sitzplaetz belegt").end();
 });
-app.get('/bars/:id', function(req, res){
+
+app.get('/bars/:id', function(req, res){ //Rückgabe der bar mittels bar/id
    db.get('bars:'+req.params.id, function(err, rep){
        if(rep){
            res.type('json').send(rep);
        }
        else{
-           res.status(404).type('text').send("Die Bar mit der ID" + rep.params.id + " wurde nicht gefunden");
+           res.status(404).type('text').send("Die Bar mit der ID " + req.params.id + " wurde nicht gefunden");
        }
    });
 });
+
+app.get('/bars/:id/details', function(req, res){ //Rückgabe der Details der Bar mittels id
+   db.get('bars:'+req.params.id+'/details', function(err, rep){
+       if(rep){
+           res.type('json').send(rep);
+       }
+       else{
+           res.status(404).type('text').send("Die Bar mit der ID " + req.params.id + " wurde nicht gefunden");
+       }
+   });
+});
+
 app.get('/', function(req, res){
     res.send("Von " + sitzplaetze + " Sitzplaetze sind \n" + freieSitzplaetze + " freie Sitzplaetze vorhanden").end();
 });
