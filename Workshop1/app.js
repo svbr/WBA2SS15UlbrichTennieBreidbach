572b5415cd257e72a50c1d8f1f8d0eaa0347bec5
ktennie <Keno@p218099.vpn-dyn.FH-Koeln.DE>
2015-06-02 13:01:50 +0200

    details und name der bar können abgerufen werden

--- Workshop1/app.js
+++ Workshop1/app.js
@@ -29,7 +29,7 @@ app.post('/user', function(req, res){
     
     db.incr('id:user', function(err, rep){
         newUser.id = rep;
-        db.set('user:'+ newUser.id, newUser,function(err, rep){
+        db.set('user:'+ newUser.id, JSON.stringify(newUser),function(err, rep){
 			res.send("Der User " + JSON.stringify(newUser.name) + " mit der ID " + JSON.stringify(newUser.id) + " wurde hinzugefügt!").end();
 		});
     });
@@ -45,7 +45,7 @@ app.post('/user/:id/bars',function(req, res){ //Hinzufügen von einer Bar (Ausga
         if(rep){
             db.incr('id:bars', function(err, rep){
                 newBar.id = rep;
-                db.set('bars:'+newBar.id, newBar,function(err, rep){
+                db.set('bars:'+newBar.id, JSON.stringify(newBar),function(err, rep){
 			         res.send("Die Bar " + JSON.stringify(newBar.name) + " mit der ID " + newBar.id + " wurde von dem User mit der ID " + req.params.id + " hinzugefügt!").end();
                 });
             });
@@ -56,9 +56,9 @@ app.post('/user/:id/bars',function(req, res){ //Hinzufügen von einer Bar (Ausga
     });
 });
 
-app.post('/user/:id/bars/:id/details', function(req, res){ //Hinzufügen der Sitzplätze einer bestimmten Bar
+app.post('/bars/:id/details', function(req, res){ //Hinzufügen der Sitzplätze einer bestimmten Bar
     var newAnzahl = req.body;
-    db.get('user:'+req.params.id, function(err, rep){
+    db.get('bars:'+req.params.id, function(err, rep){
        if(rep){
            db.set('bars:'+req.params.id+'/details', JSON.stringify(newAnzahl),function(err, rep){
 			 res.send("Die Details wurden der ID " + req.params.id + " hinzugefügt!").end();
@@ -70,7 +70,13 @@ app.post('/user/:id/bars/:id/details', function(req, res){ //Hinzufügen der Sit
    });
 })
 
-app.get('/user/:id', function(err, req){
+/*app.put(...., function(){
+    --> userrechte werden überprüft
+    --> daten werden barbeitet... je nach dem was im JSON-Objekt dinn ist
+    --> änderung werden gespeichert
+});*/
+
+app.get('/user/:id', function(err, res){
     db.get('user:'+req.params.id, function(err, rep){
        if(rep){
            res.type('json').send(rep);
@@ -85,12 +91,20 @@ app.get('/user/:id', function(err, req){
 app.get('/bars/:id', function(req, res){ //Rückgabe der Bar mittels bar/id
    db.get('bars:'+req.params.id, function(err, rep){
        if(rep){
-           res.send("Barname:").end();
+           res.type('json').send(rep);
        }
        else{
            res.status(404).type('text').send("Die Bar mit der ID " + req.params.id + " wurde nicht gefunden");
-       }
+     }
    });
+});
+
+app.get('/bars/:id/aktuell', function(req, res){
+    //db.get --> liefert json-Objekt, das die bardaten enthält (bar, sitzplätze, öffnungszeiten, karte, Event)
+        //--> aktuelles Datum muss ermittelt werden
+        //--> daten verwerten und auf aktuelles Datum angepasst werden
+    //ausgebe durch res.send
+
 });
 
 app.get('/bars/:id/details', function(req, res){ //Rückgabe der Details der Bar mittels id
