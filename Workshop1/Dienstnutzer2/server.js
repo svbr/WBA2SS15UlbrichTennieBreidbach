8a344ed263ae9deb558e219309bfdc18174fcee1
Sven Breidbach <sven.breidbach@web.de>
2015-08-19 11:11:53 +0200

    User Registrieren hinzugefügt

--- Workshop1/Dienstnutzer2/server.js
+++ Workshop1/Dienstnutzer2/server.js
@@ -75,6 +75,47 @@ app.get('/about', function(req, res) {
 app.get('/login', function(req, res) {
 	res.render('./pages/login.ejs');
 });
+
+//User anlegen / registrieren
+app.get('/register', function(req, res) {
+	res.render('./pages/register.ejs');
+});
+
+app.post('/register', function(req, res){
+    var test = req.body;
+    console.log(test);
+   fs.readFile("./views/pages/register.ejs", {encoding:"utf-8"}, function(err, filestring){
+    if(err){
+      throw err;
+    } else{
+      var options = {
+        host: "localhost",
+        port: 3000,
+        path: "/user",
+        method:"POST"
+      }
+			var externalRequest = http.request(options, function(externalResponse){
+        console.log("Connected User post");
+				externalResponse.on("data", function(chunk){
+          var newUser = JSON.parse(chunk);
+					var html = ejs.render(filestring, {newUser: newUser, filename: __dirname + '/views/pages/register.ejs'});
+					res.setHeader("content-type", "text/html");
+          res.writeHead(200);
+          res.write(html);
+          res.end();
+				});
+			});
+			externalRequest.on('error', function(e) {
+        console.log('problem with request: ' + e.message);
+      });
+      externalRequest.setHeader("content-type", "application/json");
+      externalRequest.write(JSON.stringify(test));
+      console.log(test);
+      externalRequest.end();
+    }
+});
+});
+
 // Alle bars auflisten
 app.get('/bars', function(req, res){
     fs.readFile("./views/pages/bars.ejs", {encoding:"utf-8"}, function(err, filestring){
@@ -93,10 +134,10 @@ app.get('/bars', function(req, res){
       var externalRequest = http.request(options, function(externalResponse){
         console.log("Connected Bars get");
         if(externalResponse.statusCode == 404){
-            
+
         } else {
             externalResponse.on("data", function(chunk){
-            
+
             var bars = JSON.parse(chunk);
             var html = ejs.render(filestring, {bars: bars, filename: __dirname + '/bars.ejs'});
             res.setHeader("content-type", "text/html");
@@ -130,7 +171,7 @@ app.get("/bars/:bid", function(req, res, next){
       var externalRequest = http.request(options, function(externalResponse){
         console.log("Connected Bars get");
         externalResponse.on("data", function(chunk){
-          var bars = JSON.parse(chunk); 
+          var bars = JSON.parse(chunk);
           var html = ejs.render(filestring, {bars: bars, filename: __dirname + '/bars.ejs'});
           res.setHeader("content-type", "text/html");
           res.writeHead(200);
@@ -175,9 +216,10 @@ app.get("/user/:id", function(req, res, next){
 
 app.get('/add', function(req, res){
     res.render('./pages/add.ejs');
-    
+
 });
 
+//Bar anlegen
 app.post('/add', function(req, res){
     var test = req.body;
     console.log(test);
